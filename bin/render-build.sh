59f22d9e5e7a1d634ac9b65cb3b19631c63d31ba
ryochi0055 <aredoggu@gmail.com>
2024-08-21 18:34:03 +0900

    Create render-build.sh

---
+++ bin/render-build.sh
@@ -0,0 +1,5 @@
+touch ./bin/render-build.sh
+echo '#!/bin/bash' > ./bin/render-build.sh
+echo 'echo "Building project..."' >> ./bin/render-build.sh
+echo 'npm run build' >> ./bin/render-build.sh
+chmod +x ./bin/render-build.sh
