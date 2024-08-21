14e8491befbd6e450b2a2f48c9bafd06d7bf8ec2
ryochi0055 <aredoggu@gmail.com>
2024-08-21 18:28:33 +0900

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,4 +1,3 @@
-touch ./bin/render-build.sh
-echo '#!/bin/bash' > ./bin/render-build.sh
-echo 'echo "Building project..."' >> ./bin/render-build.sh
-echo 'npm run build' >> ./bin/render-build.sh
+#!/bin/bash
+echo "Building project..."
+npm run build
